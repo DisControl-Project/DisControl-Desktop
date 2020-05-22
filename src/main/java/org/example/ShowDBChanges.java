fee2fbbbbc3d5b03918be08ab8e54eaa8ce62af1
Jona <jona2255@gmail.com>
2020-05-22 20:45:25 +0200

    Beta 0.1

--- src/main/java/org/example/ShowDBChanges.java
+++ src/main/java/org/example/ShowDBChanges.java
@@ -4,6 +4,7 @@ import com.google.firebase.database.*;
 
 import java.awt.*;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 
 public class ShowDBChanges implements Runnable {
@@ -11,37 +12,94 @@ public class ShowDBChanges implements Runnable {
     int oldY = 0;
     int newX;
     int newY;
-    boolean clickL = false;
-    boolean clickR = false;
-
+    int tamanoTexto = 0;
     Robot robot = new Robot();
 
     public ShowDBChanges() throws AWTException {
+
     }
 
     public void move(int x, int y){
         robot.mouseMove(MouseInfo.getPointerInfo().getLocation().x + x, MouseInfo.getPointerInfo().getLocation().y + y);
     }
 
-    public void clikL(boolean clickL){
-        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 
-        this.clickL = false;
+    public void clikL(boolean clickL){
+        if (clickL){
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        }
+
+
     }
     public void clickR(boolean clickR){
-        robot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
-        robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
-
-        this.clickR = false;
+        if (clickR) {
+            robot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
+            robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+        }
     }
-    public void write(){
+    public void write(String s){
 
+        char caracter = s.charAt(s.length() - 1);
+        int ascii = caracter;
 
+        System.out.println(caracter);
+        System.out.println(ascii);
+
+        if (s.length() < tamanoTexto) {
+            robot.keyPress(KeyEvent.VK_BACK_SPACE);
+            robot.keyRelease(KeyEvent.VK_BACK_SPACE);
+
+        } else if (Character.isUpperCase(caracter)){
+            robot.keyPress(KeyEvent.VK_SHIFT);
+            robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(ascii));
+            robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(ascii));
+            robot.keyRelease(KeyEvent.VK_SHIFT);
+
+        } else {
+            robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(ascii));
+            robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(ascii));
+
+        }
+
+        tamanoTexto = s.length();
+
+    }
+    public void borrar(){
+        robot.keyPress(KeyEvent.VK_BACK_SPACE);
+        robot.keyRelease(KeyEvent.VK_BACK_SPACE);
+    }
+    public void intro(){
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+    }
+    public void flechasDireccion(String direccion){
+
+        if (direccion.equals("up")){
+
+            robot.keyPress(KeyEvent.VK_UP);
+            robot.keyRelease(KeyEvent.VK_UP);
+
+        } else if (direccion.equals("left")){
+
+            robot.keyPress(KeyEvent.VK_LEFT);
+            robot.keyRelease(KeyEvent.VK_LEFT);
+
+        }else if (direccion.equals("right")){
+
+            robot.keyPress(KeyEvent.VK_RIGHT);
+            robot.keyRelease(KeyEvent.VK_RIGHT);
+
+        } else {
+
+            robot.keyPress(KeyEvent.VK_DOWN);
+            robot.keyRelease(KeyEvent.VK_DOWN);
+
+        }
     }
 
 
-   // cuando se hace tap oldX y oldY sea igual a newX y newY
+    // cuando se hace tap oldX y oldY sea igual a newX y newY
     public void run() {
         FireBaseService fbs = null;
         try {
@@ -61,18 +119,42 @@ public class ShowDBChanges implements Runnable {
                 newX = (int) Double.parseDouble(dataSnapshot.child("usuario").child("pos").child("x_pos").getValue().toString());
                 newY = (int) Double.parseDouble(dataSnapshot.child("usuario").child("pos").child("y_pos").getValue().toString());
 
-                if (dataSnapshot.child("usuario").child("clicks").child("clickL").getValue().toString().equals("true")){
-                    clickL = true;
-                    clikL(clickL);
-                }
-                if (dataSnapshot.child("usuario").child("clicks").child("clickR").getValue().toString().equals("true")){
-                    clickR = true;
-                    clickR(clickR);
-                }
+                clikL(dataSnapshot.child("usuario").child("clicks").child("clickL").getValue().toString().equals("true"));
+
+                clickR(dataSnapshot.child("usuario").child("clicks").child("clickR").getValue().toString().equals("true"));
+
                 move(oldX+newX, oldY+newY);
 
                 oldX = newX;
                 oldY = newY;
+
+                if (dataSnapshot.child("usuario").child("texto").child("borrar").getValue().toString().equals("true")){
+                    borrar();
+                }
+                if (dataSnapshot.child("usuario").child("texto").child("intro").getValue().toString().equals("true")){
+                    intro();
+                }
+
+                if(dataSnapshot.child("usuario").child("clicks").child("up").getValue().toString().equals("true")){
+
+                    flechasDireccion("up");
+
+                } else if(dataSnapshot.child("usuario").child("clicks").child("left").getValue().toString().equals("true")){
+
+                    flechasDireccion("left");
+
+                }else if(dataSnapshot.child("usuario").child("clicks").child("down").getValue().toString().equals("true")){
+
+                    flechasDireccion("down");
+
+                }else if(dataSnapshot.child("usuario").child("clicks").child("right").getValue().toString().equals("true")){
+
+                    flechasDireccion("right");
+
+                }
+
+                write(dataSnapshot.child("usuario").child("texto").child("texto").getValue().toString());
+
 
 
 
